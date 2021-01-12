a2484c047758638c0b75ee9604587ca70d0accb5
pinktoothbrush13 <jinandreiparcia13@gmail.com>
2021-01-12 21:03:18 +0800

    Initial commit

---
+++ app/src/main/java/mcm/edu/ph/chupapimonyanyo/splashscreen.java
@@ -0,0 +1,14 @@
+package mcm.edu.ph.chupapimonyanyo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class splashscreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splashscreen);
+    }
+}
